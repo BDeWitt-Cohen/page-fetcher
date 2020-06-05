7a738a0e51ba899ecff1c1a89f1d4ec8f7a33532
Trevor Thomas <thomastrevor1@gmail.com>
2020-06-05 14:47:33 +0000

    fetcher with all associated files

--- fetcher.js
+++ fetcher.js
@@ -1,34 +1,47 @@
 const request = require('request');
 const fs = require('fs');
 let args = process.argv.slice(2);
+const readline = require("readline");
+// const rl = readline.createInterface({
+//     input: process.stdin,
+//     output: process.stdout
+// });
 
 let domainName = args[0];
 let dlPath = args[1];
 // /vagrant/w2/d3-net/page-fetcher
 
-
 request(domainName, (error, response, body) => {
   if (error) {
-    console.log(`Looks like that there was this error: ${error}`);
+    console.log('There was an error downloading from the domainName: ', error);
   }
-  // if (dlPath){
-  //   fs.access('./index.html', (err) => {
-  //     fs.writeFile(dlPath, body, function(err) {
-  //       fs.stat(dlPath, (err, stat) => {
-  //         console.log(`File already existed but we Downloaded and saved ${stat.size} bytes to ${dlPath}`);
-  //       })
-  //     });
-  // });
-  // }
-  if (!dlPath) {
-    console.log("Sorry, your download path doesn't work");
-  } else
-    fs.writeFile(dlPath, body, function(err) {
-      fs.stat(dlPath, (err, stat) => {
-        console.log(`Downloaded and saved ${stat.size} bytes to ${dlPath}`);
-      });
-    });
+
+  fs.access(dlPath, (err) => {
+    if (err) {
+      consoile.log('Unable to save file to download path.', err);
+    }
+
+    fs.exists(`${dlPath}index.html`, (fileExists) => {
+      if (fileExists) {
+        fs.writeFile(`${dlPath}index.html`, body, function(err) {
+          if (err) {
+            console.log('There was an error saving the file: ', err);
+          }
+          fs.stat(dlPath, (err, stat) => {
+            console.log(`File overwritten and saved ${stat.size} bytes to ${dlPath}index.html`)
+          });
+        });
+      } else {
+        fs.writeFile(`${dlPath}index.html`, body, function(err) {
+          if (err) {
+            console.log('There was an error saving the file: ', err);
+          }
+          fs.stat(dlPath, (err, stat) => {
+            console.log(`File downloaded and saved ${stat.size} bytes to ${dlPath}index.html`)
+          });
+        });
+      }
+    })
+  });
 });
-
-
 
